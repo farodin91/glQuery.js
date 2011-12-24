e848d4d5d0abc440b3554877616865a257bd26b5
farodin91 <farodin91@googlemail.com>
2011-12-24 17:04:09 +0100

    Update r2 Some silly fales fixes
    Signed-off-by: farodin91 <farodin91@googlemail.com>

---
+++ engine/object/glQuery.object.js
@@ -0,0 +1,264 @@
+/*
+ * Copyright 2011, Jan Jansen
+ * Licensed under the  GPL Version 3 licenses.
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ * 
+ *@fileOverview
+ *@name glQuery.object.js
+ *@author Jan Jansen - farodin91@googlemail.com
+ *@description Coming soon
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * Depends:
+ *	jquery.1.5.0.js
+ *	jquery.ui.core.js
+ *	jquery.ui.widget.js
+ *	jquery.ui.mouse.js
+ *	sylvester.src.js
+ *	glQuery.core.js
+ *	glQuery.collada.js
+ *	glQuery.input.js
+ *	glQuery.scene.js
+ *	glQuery.events.js
+ *	glQuery.math.js
+ *	glQuery.webgl.js
+ *	glQuery.animation.js
+ *	glQuery.physics.js
+ *	glQuery.textures.js
+ */
+(function( glQuery, undefined ) {
+
+    glQuery.objects = {
+        init:function(){
+            this.objectWorker = new Worker(glQuery.options.partToglQuery+"engine/worker/glQuery.object.worker.js");
+            
+            this.objectWorker.onmessage = function(event){
+                switch (event.data.type){
+                    case "returnObjects":
+                        glQuery.selectors[event.data.selector] = event.data.object;
+                        break;
+                }
+            }
+            
+        },
+        
+        /**
+         * @function add
+         * 
+         * @description create the render by an event of the renderWorker
+         * 
+         * @param colladaObject object
+         * @param id string
+         * @param type string
+         * @param art string
+         * @param position object
+         * 
+         */
+        add:function(colladaObject,id,type,art,position){
+            log.debug("glQuery.objects.add() start");
+            var self = this;/*
+            var obj = {};
+            obj.Buffers = this.createObjectBuffer(colladaObject);
+            obj.Id = id;
+            obj.Show = true;
+            obj.Find = true;
+            obj.mvMatrix = mat4.create();//replace with lookat
+            obj.mvMatrix = mat4.identity(obj.mvMatrix); 
+            obj.mvMatrix = mat4.scale(obj.mvMatrix, colladaObject.Object.Scale);
+            obj.mvMatrix = mat4.translate(obj.mvMatrix, colladaObject.Object.Translate);
+            
+            obj.mvMatrix = mat4.rotateX(obj.mvMatrix, colladaObject.Object.Rotate[0]);
+            obj.mvMatrix = mat4.rotateY(obj.mvMatrix, colladaObject.Object.Rotate[1]);
+            obj.mvMatrix = mat4.rotateZ(obj.mvMatrix, colladaObject.Object.Rotate[2]);
+            //obj.nMatrix  = mat4.inverse(obj.mvMatrix);
+            //obj.nMatrix  = mat4.transpose(obj.mvMatrix);
+            delete colladaObject;
+            
+            this.object[Id]= obj;*/
+            
+            var object = new NormObject(id);
+            object.setType(type);
+            object.setArt(art);
+            object.setViewAble(true);
+            object.setVec3WorldPos(position)
+            object.rotateMvMat4(colladaObject.Object.Rotate[0], colladaObject.Object.Rotate[1], colladaObject.Object.Rotate[2]);
+            object.scaleMvMat4(colladaObject.Object.Scale);
+            object.setBuffers(this.createObjectBuffers(colladaObject));
+            
+            this.object[(this.i-1)] = object;
+            
+            log.debug("glQuery.objects.add() finish");
+            return true;
+        },
+        createObjectBuffers:function(colladaObject){
+            var Buffers = {};
+            
+            
+            Buffers.VerticesBuffer = glQuery.gl.createBuffer();
+            glQuery.gl.bindBuffer(glQuery.gl.ARRAY_BUFFER, Buffers.VerticesBuffer);
+            glQuery.gl.bufferData(glQuery.gl.ARRAY_BUFFER, new Float32Array(colladaObject.Vertex.Positions.array), glQuery.gl.STATIC_DRAW);
+            
+
+            
+            /*
+            if (glQuery.webGL.aVertexNormal != -1) {
+                
+                Buffers.normal = glQuery.gl.createBuffer();
+                glQuery.gl.bindBuffer(glQuery.gl.ARRAY_BUFFER, Buffers.normal);
+                glQuery.gl.bufferData(glQuery.gl.ARRAY_BUFFER, new Float32Array(colladaObject.Vertex.Normals.array), glQuery.gl.STATIC_DRAW);
+                
+                Buffers.NormalIndexBuffer = glQuery.gl.createBuffer();
+                glQuery.gl.bindBuffer(glQuery.gl.ELEMENT_ARRAY_BUFFER, Buffers.NormalIndexBuffer);
+                glQuery.gl.bufferData(glQuery.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(colladaObject.Vertex.Index["NORMAL"]), glQuery.gl.STATIC_DRAW);
+            }*/
+            
+            /*
+            if (ta != -1) {
+                Buffers.texcoord = glQuery.gl.createBuffer();
+                glQuery.gl.bindBuffer(glQuery.gl.ARRAY_BUFFER, Buffers.texcoord);
+                glQuery.gl.bufferData(glQuery.gl.ARRAY_BUFFER, new Float32Array(sf.mesh.texcoord), glQuery.gl.STATIC_DRAW);
+            }*/
+            
+            Buffers.VertexNum = colladaObject.Vertex.Positions.num
+            Buffers.vPos = [1,1,1];
+            Buffers.bbox = colladaObject.Vertex.bbox;
+            Buffers.itemSize = 3;
+            Buffers.numItems = colladaObject.Vertex.Positions.num;
+            Buffers.numIndices = colladaObject.Vertex.Index["VERTEX"].length;
+            
+        
+            Buffers.IndexBuffer = glQuery.gl.createBuffer();
+            glQuery.gl.bindBuffer(glQuery.gl.ELEMENT_ARRAY_BUFFER, Buffers.IndexBuffer);
+            glQuery.gl.bufferData(glQuery.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(colladaObject.Vertex.Index["VERTEX"]), glQuery.gl.STATIC_DRAW);
+            
+            
+            return Buffers;
+        },
+        getObjectById:function(Id,selector){
+            glQuery.renderWorker.postMessage({"action":"getObjectById",get:Id,context:glQuery.selectors[selector],selector:selector});
+            return true;
+        },
+        getObjectByArt:function(Art,selector){
+            glQuery.renderWorker.postMessage({"action":"getObjectByArt",get:Art,context:glQuery.selectors[selector],selector:selector});            
+            return true;
+        },
+        getObjectByType:function(Type,selector){
+            glQuery.renderWorker.postMessage({"action":"getObjectByType",get:Type,context:glQuery.selectors[selector],selector:selector});            
+            return true;
+        },
+        duplicate:function(){},
+        objectWorker:null,
+        object:[],
+        i:0,
+        camera:{}        
+    };
+    
+    
+    NormObject = function(id){
+        this.id             = 0;
+        this.i              = 0;
+        this.type           = "";
+        this.art            = "";
+        this.buffers        = {};
+        this.viewAble       = true;
+        this.mvMat4         = mat4.create();
+        this.vWorldPos      = vec3.create();
+        this.scaleVec3      = vec3.create();
+        this.rotateX        = 0;
+        this.rotateY        = 0;
+        this.rotateZ        = 0;
+        
+        
+        this.id             = id;
+        this.i              = glQuery.objects.i;
+        glQuery.objects.i   = glQuery.objects.i + 1;
+        this.mvMat4         = mat4.identity(this.mvMat4); 
+        
+        this.setBuffers = function(buffers){
+            this.buffers = buffers;
+        };
+        this.getBuffers = function(){
+            return this.buffers;
+        };
+        
+        this.setType = function(type){
+            this.type = type;
+            if(this.getArt() != ""){
+                glQuery.objects.objectWorker.postMessage({
+                    id:this.id,
+                    type:this.type,
+                    art:this.art,
+                    i:this.i,
+                    action:"add"
+                });
+            }
+        };
+        this.getType = function(){
+            return this.type;
+        };
+        
+        this.setArt = function(art){
+            this.art = art;
+            if(this.getType() != ""){
+                glQuery.objects.objectWorker.postMessage({
+                    id:this.id,
+                    type:this.type,
+                    art:this.art,
+                    i:this.i,
+                    action:"add"
+                });
+            }
+        };
+        this.getArt = function(){
+            return this.art;
+        };
+        
+        
+        this.setViewAble = function(viewAble){
+            this.viewAble = viewAble;
+        };
+        this.getViewAble = function(){
+            return this.viewAble;
+        };
+        
+        this.setMvMat4 = function(mvMat4){
+            this.mvMat4 = mat4.create(mvMat4);	
+        };
+        this.getMvMat4 = function(){
+            return this.mvMat4;
+        };
+        this.scaleMvMat4 = function(vec){
+            this.scaleVec3 = vec3.create(vec);
+            this.mvMat4 = mat4.scale(this.mvMat4, vec);
+        };
+        this.setVec3WorldPos = function(vec){
+            if(!vec)
+                vec = [0,0,0];
+            this.vWorldPos = vec3.create(vec);
+        };
+        this.getVec3WorldPos = function(){
+            return this.vWorldPos;
+        };
+        this.rotateMvMat4 = function(rotateX,rotateY,rotateZ){
+            this.rotateX = rotateX;
+            this.rotateY = rotateY;
+            this.rotateZ = rotateZ;
+            this.mvMat4 = mat4.rotateX(this.mvMat4, rotateX);
+            this.mvMat4 = mat4.rotateY(this.mvMat4, rotateY);
+            this.mvMat4 = mat4.rotateZ(this.mvMat4, rotateZ);            
+        }
+        return this;
+    };
+    
+    NormBuffer = function(){
+        this.itemSize = 3;
+        
+        return this;
+    };
+
+})(glQuery);
---
+++ engine/webGL/glQuery.shader.js
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2011, Jan Jansen
+ * Licensed under the  GPL Version 3 licenses.
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ * 
+ *@fileOverview
+ *@name glQuery.shader.js
+ *@author Jan Jansen - farodin91@googlemail.com
+ *@description Coming soon
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * Depends:
+ *	jquery.1.5.0.js
+ *	jquery.ui.core.js
+ *	jquery.ui.widget.js
+ *	jquery.ui.mouse.js
+ *	sylvester.src.js
+ *	glQuery.core.js
+ *	glQuery.collada.js
+ *	glQuery.input.js
+ *	glQuery.scene.js
+ *	glQuery.webgl.js
+ *	glQuery.events.js
+ *	glQuery.math.js
+ *	glQuery.animation.js
+ *	glQuery.object.js
+ *	glQuery.physics.js
+ *	glQuery.textures.js
+ */
+
+(function( glQuery, undefined ) {
+
+    glQuery.shader = {
+        createVertexShader: function(){
+            var op = glQuery.webGL.options;
+            if(op){
+                
+            }
+            var shader = "\n";
+            //shader += "attribute    highp       vec3 aVertexNormal;\n"
+            shader += "attribute    highp       vec3 aVertex;\n";
+
+
+            shader += "uniform                  mat4 uPMatrix;\n";
+            shader += "uniform                  mat4 uMVMatrix;\n";
+            //shader += "uniform      highp       mat4 uNormalMatrix;\n";
+            
+            shader += "uniform      highp       vec3 vWorldPos;\n";
+            shader += "uniform      highp       vec3 vCameraPos;\n";
+            /*
+            shader += "uniform      highp       vec3 uAmbientLight;\n";
+            shader += "uniform      highp       vec3 uDirectionalLightColor;\n";
+            shader += "uniform      highp       vec3 uDirectionalVector;\n";
+            
+            shader += "uniform      highp       vec4 uSpecularColor;\n"
+            shader += "uniform      highp       vec4 uDiffuseColor;\n"
+            
+                  
+            shader += "varying      highp       vec3 vLighting;\n"
+            shader += "varying      highp       vec4 vSpecularColor;\n"
+            shader += "varying      highp       vec4 vDiffuseColor;\n"*/
+
+            shader += "void main(void) {\n";
+        
+            shader += "  gl_Position = uPMatrix * uMVMatrix * vec4(aVertex, 1.0);\n";
+            
+            
+            //shader += "  highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);\n";
+            /*
+            shader += "  highp float directional = max(dot(transformedNormal.xyz, uDirectionalVector), 0.0);\n";
+            if(op.fog){
+                
+            }
+            shader += "  vLighting = uAmbientLight + (uDirectionalLightColor * directional);\n";
+            
+            shader += "  vSpecularColor = uSpecularColor;\n";   
+            shader += "  vDiffuseColor = uDiffuseColor;\n";*/
+
+            
+            shader += "}";
+            
+            
+            
+            return shader;
+        },
+        createFragmentShader: function(){
+            var shader = "\n";/*
+            shader += "varying      highp       vec3    vLighting;\n"
+            shader += "varying      highp       vec4    vDiffuseColor;\n"
+            shader += "varying      highp       vec4    vSpecularColor;\n"*/
+            
+            shader += "void main(void) {\n";
+            
+            shader += "  gl_FragColor = vec4(0.1, 0.1, 0.1, 1.0);\n";
+            
+            shader += "}";
+            return shader;
+        }
+        
+    };
+})(glQuery);
