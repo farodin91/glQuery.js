f9e0a4343f08225d6f16a3859e086611a079c80b
farodin91 <farodin91@googlemail.com>
2012-02-09 15:27:59 +0100

    Update r32: new Collada Structure (Part 7)
    Signed-off-by: farodin91 <farodin91@googlemail.com>

--- engine/extra/parser/collada/glQuery.material.collada.js
+++ engine/extra/parser/collada/glQuery.material.collada.js
@@ -39,7 +39,7 @@
                         break;
                 }
             });
-            return {};
+            return material;
         },
         techniqueCommon:function(node){
             var self = this;
@@ -53,7 +53,7 @@
                         
                 }
             })
-            return {};
+            return material;
         },
         instanceMaterial:function(node){
             var self = this;
@@ -70,18 +70,179 @@
                     
                 }
             });
-            return {};
+            return material;
         },
         getMaterial:function(node){
             var self = this;
             var material = {}
-            
-            return {};
+            node.find("> *").each(function(){
+                switch(this.nodeName){
+                    case "asset"://Coming Soon!
+                        break;
+                    case "instance_effect":
+                        material.effect = self.instanceEffect(this);
+                        break;
+                    case "extra"://Coming Soon!
+                        break;
+                    
+                }
+            });
+            return material;
         },
-        bindStandardMaterial:function(){
+        instanceEffect:function(node){
             var self = this;
-            var material = {}
-            return {};
-        }
-    };
+            var effect = {}
+            effect = this.getEffect(glQuery.collada.parseURI(node.getAttribute("url"),this.data));
+            jQuery(node).find("> *").each(function(){
+                switch(this.nodeName){
+                    case "technique_hint"://Coming Soon!
+                        break;
+                    case "setparam":
+                        break;
+                    case "extra"://Coming Soon!
+                        break;
+                    
+                }
+            });
+            
+            return effect;
+        },
+        getEffect:function(node){
+            var self = this;
+            var effect = {}
+            node.find("> *").each(function(){
+                switch(this.nodeName){
+                    case "asset"://Coming Soon!
+                        break;
+                    case "annotate"://Coming Soon!
+                        break;
+                    case "profile_BRIDGE"://Coming Soon!
+                        break;
+                    case "profile_CG"://Coming Soon!
+                        break;
+                    case "profile_GLES"://Coming Soon!
+                        break;
+                    case "profile_GLES2"://Coming Soon!
+                        break;
+                    case "profile_GLSL"://Coming Soon!
+                        break;
+                    case "profile_COMMON":
+                        effect.common = self.getProfileCOMMON(this);
+                        break;
+                    case "newparam"://Coming Soon!
+                        break;
+                    case "extra"://Coming Soon!
+                        break;
+                    
+                }
+            });
+            return effect;
+        },
+        getProfileCOMMON:function(node){
+            var self = this;
+            var profil = {}
+            jQuery(node).find("> *").each(function(){
+                switch(this.nodeName){
+                    case "asset"://Coming Soon!
+                        break;
+                    case "annotate"://Coming Soon!
+                        break;
+                    case "blinn"://Coming Soon!
+                        break;
+                    case "constant"://Coming Soon!
+                        break;
+                    case "lambert":
+                        break;
+                    case "phong":
+                        profil.phong = {};
+                        profil.phong = self.getPhong(this);
+                        break;
+                    case "pass"://Coming Soon!
+                        break;
+                    case "extra"://Coming Soon!
+                        break;                    
+                }
+            })
+            return profil;
+        },
+        getPhong:function(node){
+            var self = this;
+            var phong = {}
+            jQuery(node).find("> *").each(function(){
+                switch(this.nodeName){
+                    case "emission":
+                    case "ambient":
+                    case "diffuse":
+                    case "specular":
+                    case "transparent":
+                    case "reflective":
+                        var profil =self.getCommonColorOrTexture(this);
+                        if(profil.type)
+                        break;
+                    case "reflectivity":
+                    case "transparency":
+                    case "shininess":
+                    case "index_of_refraction":
+                        phong[this.nodeName]=self.getCommonFloat(this);
+                        break;
+                    
+                }
+            });
+            
+            return phong;
+        },
+        getCommonFloat:function(node){
+            var Float = 0
+            var child = node.firstElementChild;
+            if(child.nodeName == "float"){
+                Float = parseFloat(child.textContent);
+            }else{
+                Float = this.getCommonParam(child.getAttribute("ref"));
+            }
+            return Float;
+        },
+        getCommonColorOrTexture:function(node){
+            var self = this;
+            var profil = {};
+            var child = node.firstElementChild;
+            switch(child.nodeName){
+                case "color":
+                    profil.type = "color";
+                    profil.color = glQuery.collada.parseFloatArray(child.textContent);
+                    break;
+                case "texture":
+                    profil.type = "texture";
+                    profil.texture = glQuery.collada.instanceTexture(child,self);
+                    break;
+                case "param":
+                    profil = this.getCommonParam(child.getAttribute("ref"));
+                    break;
+            }
+            
+        },
+        getCommonParam:function(sid){
+            var node = jQuery("[sid="+sid+"]");
+            var param;
+            node.find("> *").each(function(){
+                switch(this.nodeName){
+                    case "float":
+                        param = parseFloat(this.textContent);
+                        break;
+                    case "float2":
+                    case "float3":
+                    case "float4":
+                        param = glQuery.collada.parseFloatArray(this.textContent);
+                        break;
+                    case "sampler2D":
+                        break;
+                }
+        })
+        return param;
+    },
+    bindStandardMaterial:function(){
+        var self = this;
+        var material = {}
+        return {};
+    }
+};
 })(glQuery );
