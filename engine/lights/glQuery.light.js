d7e9fe06cf34385e31b5f1be93339495376cb76f
farodin91 <farodin91@googlemail.com>
2012-04-29 22:16:33 +0200

    Update r42: starting the light technique implementation
    Signed-off-by: farodin91 <farodin91@googlemail.com>

--- engine/lights/glQuery.light.js
+++ engine/lights/glQuery.light.js
@@ -22,12 +22,82 @@
     
     glQuery.light = {
         shaderLight:{
-                MAX_SPOT_LIGHTS                         :0,
-                MAX_POINT_LIGHTS                        :0,
-                MAX_DIR_LIGHTS                          :0            
+            MAX_SPOT_LIGHTS                         :0,
+            MAX_POINT_LIGHTS                        :0,
+            MAX_DIR_LIGHTS                          :0            
+        },
+        lights:[],
+        art:[],
+        id:[],
+        i:0,
+        add:function(id,art,lightData,direction){
+            var light = new NormLight(id);
+            light.setArt(art);
+            switch(lightData.type){
+                case "point":
+                    break;
+                case "ambient":
+                    break;
+                case "spot":
+                    break;
+                case "directional":
+                    break;
+            }
+        },
+        getLightById:function(id,context){
+            return [this.id[id]];
+        },
+        getLightByArt:function(art,context){       
+            return this.art[art];
         },
         uniformLighting:function(shader){
             
         }
+    };
+    NormLight = function(id){
+        this.id                 = 0;
+        this.i                  = 0;
+        this.type               = "light";
+        this.art                = "";
+        this.light              = {};
+        this.viewAble           = true;
+        this.shaderProgramKey   = -1;
+        
+        
+        this.id             = id;
+        this.i              = glQuery.light.i;
+        glQuery.light.i     = glQuery.light.i + 1;
+        
+        glQuery.light.id[id]  = this.i;
+        
+        this.getType = function(){
+            return this.type;
+        };
+        
+        this.setArt = function(art){
+            this.art = art;
+            if(!glQuery.object.art[this.art]){
+                glQuery.object.art[this.art] = [];
+            }
+            glQuery.object.art[this.art][glQuery.object.art[this.art].length] = this.i;
+        };
+        this.getArt = function(){
+            return this.art;
+        };
+        
+        
+        this.setViewAble = function(viewAble){
+            this.viewAble = viewAble;
+        };
+        this.getViewAble = function(){
+            return this.viewAble;
+        };
+        return this;
+    };
+    
+    NormBuffer = function(){
+        this.itemSize = 3;
+        
+        return this;
     };
 })(glQuery );
